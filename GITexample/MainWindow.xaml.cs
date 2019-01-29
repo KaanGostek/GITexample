91db39fe729020e30909a6eace2a75e51a7ce440
Kaan Gostek <kaan.gostek@sogeti.com>
2019-01-29 11:38:29 +0100

    plus function works

--- GITexample/MainWindow.xaml.cs
+++ GITexample/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace GITexample
 {
@@ -23,6 +24,25 @@ namespace GITexample
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            if (regex.IsMatch(e.Text))
+            {
+                Console.WriteLine("yupers");
+                e.Handled = regex.IsMatch(e.Text);
+            }
+        }
+
+        private void doCalc(object sender, TextChangedEventArgs e)
+        {
+            Console.WriteLine("got in");
+            Int32.TryParse(textbox1.Text, out int first);
+            Console.WriteLine("first: " + first);
+            Int32.TryParse(textbox2.Text, out int second);
+            Console.WriteLine("second: " + second);
+            labelAnswer.Content = first + second;
         }
     }
 }
